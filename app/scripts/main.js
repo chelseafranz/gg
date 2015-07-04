af602f4a2887bf2643e784951829bfde425c0445
Chelsea Franz <chelsea@spacestud.io>
2015-07-04 12:50:57 -0400

    angular installed

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,21 @@
-console.log('The Iron Yard Rocks');
+var myApp = angular.module('MyApp', []);
+
+(function() {
+  'use strict';
+
+  angular.module('MyApp')
+    .controller('AppCtrl', function($scope) {
+      $scope.demo = {
+        topDirections: ['left', 'up'],
+        bottomDirections: ['down', 'right'],
+
+        isOpen: false,
+
+        availableModes: ['md-fling', 'md-scale'],
+        selectedMode: 'md-fling',
+
+        availableDirections: ['up', 'down', 'left', 'right'],
+        selectedDirection: 'up'
+      };
+    });
+})();
